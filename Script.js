88ae7fd635415f96210491f310ac8c2b41eb1afe
leone <leonemunyao>
2023-03-08 17:17:07 +0300

    First commit

---
+++ Script.js
@@ -0,0 +1,29 @@
+// A variable is a name storage for data
+// Used to store goodies, products
+let message = 'Hello Leone';
+console.log(message);
+
+let message1 = 'Hello Leone.Are doing something great on javascript?'
+message1 = 'Hello Leone.';
+
+alert(message1);
+let user = 'Leon';
+let adverb = 'is';
+let verb = 'walking';
+console.log(user, adverb, verb);
+
+// Naming variables.... Variable are named using two limitation...
+// 1.The name must contain only letters, symbols, digits
+// 2.The First character must not be a digit
+// Example
+
+let userName;
+let test123;
+// camelCase is highly considered if the name contains more than one name
+// Example
+
+let userNameIdentity;
+
+// Clearing constant variables......variables that do not change 
+const myBirthday = '31.08.2001';
+console.log(myBirthday);
